62cb24672fdcd543b6802347ff5e1075a9ffb41d
csvernon <csvernon@ncsu.edu>
2019-05-01 20:39:00 -0400

    added order option

--- Bamazon.sql
+++ Bamazon.sql
@@ -8,16 +8,17 @@ CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
   item_Name VARCHAR(45) NOT NULL,
   price DECIMAL(10) NOT NULL,
+  quantity DECIMAL(10) NOT NULL,
   manufacturer VARCHAR(45) NOT NULL,
   category VARCHAR(45) NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (item_Name, price, manufacturer, category)
-VALUES ("toilet paper", "10", "Charmin", "bathroom");
+INSERT INTO products (item_Name, price, quantity, manufacturer, category)
+VALUES ("toilet paper", "10", "100", "Charmin", "bathroom");
 
-INSERT INTO products (item_Name, price, manufacturer, category)
-VALUES ("lawn chain", "35", "Cheap Chinese Knockoffs", "outdoor");
+INSERT INTO products (item_Name, price, quantity, manufacturer, category)
+VALUES ("lawn chain", "35", "2", "Cheap Chinese Knockoffs", "outdoor");
 
-INSERT INTO products (item_Name, price, manufacturer, category)
-VALUES ("55in TV", "400", "Vizio", "electronics");
+INSERT INTO products (item_Name, price, quantity, manufacturer, category)
+VALUES ("55in TV", "400", "4", "Vizio", "electronics");
--- bamazonDBConnection.js
+++ bamazonDBConnection.js
@@ -1,5 +1,10 @@
 var mysql = require("mysql");
-
+var inquirer = require("inquirer");
+var selectedProduct = "";
+var currentQuantity = "";
+var selectedAmount = "";
+var cartBalance = "";
+var itemPrice = "";
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -16,13 +21,105 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-
-  connection.query("SELECT * FROM products", function(err, results) {
-    if (err) throw err;
-    console.log(results);
-  })
-
-  connection.end();
+  // run the start function after the connection is made to prompt the user
+  start();
 });
+function start() {
+  inquirer
+    .prompt({
+      name: "which",
+      type: "list",
+      message: "Hello! Welcome to Bamazon! Where would you like to go?",
+      choices: ["Full Catalog", "Order", "Multi-hits", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.which === "Full Catalog") {
+        readProducts();
+      }
+      else if(answer.which === "Order") {
+        Order();
+      }
+      else if(answer.which === "Multi-hits") {
+        multiSearch();
+      } else{
+        connection.end();
+      }
+    });
+}
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+start();
+  });
+}
 
+function Order() {
+  inquirer
+    .prompt({
+      name: "Order",
+      type: "input",
+      message: "Please input the product ID that would you like to buy.",
+    })
+    .then(function(answer) {
+      // when finished prompting, insert a new item into the db with that info
+      var query = '"' + answer.Order + '"';
+      // console.log(answer.Order)
+      // console.log(JSON.stringify(answer));
+      connection.query("SELECT * FROM products WHERE id = " + query, function(err, results) {
+        if (err) throw err;
+        
+            if (results != ""){
+              // console.log(JSON.stringify(results));
+              selectedProduct = results[0].item_Name;
+              currentQuantity = results[0].quantity;
+              itemPrice = results[0].price;
+              console.log("You have selected " + selectedProduct);
+              OrderComplete();
+            }
+            else {console.log("Sorry, we were unable to find that product")
+            start();
+          }
+        });
+})}
+
+function OrderComplete() {
+  inquirer
+  .prompt({
+    name: "OrderComplete",
+    type: "input",
+    message: "What quantity of " + selectedProduct + " would you like to purchase?",
+  })
+  .then(function(answer) {
+    // when finished prompting, insert a new item into the db with that info
+    var query = '"' + answer.OrderComplete + '"';
+    selectedAmount = answer.OrderComplete;
+    cartBalance = itemPrice * selectedAmount;
+    var remainingQuantity = currentQuantity - selectedAmount;
+    // console.log(answer.Order)
+    // console.log(JSON.stringify(answer));
+    connection.query("UPDATE products SET ? WHERE ?",
+         [
+           {
+             quantity: remainingQuantity
+           },
+           {
+             item_Name: selectedProduct
+           }
+         ],
+     function(err, results) {
+      if (err) throw err;
+      
+          if (results != ""){
+            // console.log(JSON.stringify(results));
+            console.log("Your order of " + selectedAmount + " " + selectedProduct + " will cost a total of $" + cartBalance);
+            start();
+          }
+          else {console.log("Sorry, we were unable to find that product");
+        start();
+        }
+      });
+})}
